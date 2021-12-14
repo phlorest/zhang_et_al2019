62ae9c660c8b0fff4312a033eb990ee77b00c3a9
SimonGreenhill <simon@simon.net.nz>
2021-12-14 14:29:18 +1300

    update

--- cldfbench_zhang_et_al2019.py
+++ cldfbench_zhang_et_al2019.py
@@ -6,28 +6,27 @@ import phlorest
 class Dataset(phlorest.Dataset):
     dir = pathlib.Path(__file__).parent
     id = "zhang_et_al2019"
-
+    
     def cmd_makecldf(self, args):
         self.init(args)
-        with self.nexus_summary() as nex:
-            self.add_tree_from_nexus(
-                args,
-                self.raw_dir / '109SinoTibetanLanguages.MCC.tree',
-                nex,
-                'summary',
-                detranslate=True,
-            )
+        args.writer.add_summary(
+            self.raw_dir.read_tree(
+                '109SinoTibetanLanguages.MCC.tree', detranslate=True),
+            self.metadata,
+            args.log)
+
         posterior = self.sample(
             self.remove_burnin(
-                self.read_gzipped_text(
-                    self.raw_dir / 'Sino-Tibetan Posterior tree distribution.trees.gz'),
-                1000,
-            ),
+                self.raw_dir.read('Sino-Tibetan Posterior tree distribution.trees.gz'),
+                1000),
             detranslate=True,
             as_nexus=True)
+        args.writer.add_posterior(
+            posterior.trees.trees, 
+            self.metadata, 
+            args.log)
 
-        with self.nexus_posterior() as nex:
-            for i, tree in enumerate(posterior.trees.trees, start=1):
-                self.add_tree(args, tree, nex, 'posterior-{}'.format(i))
-
-        self.add_data(args, self.raw_dir / '109SinoTibetanLanguages_Swadesh100-matched.nex')
+        args.writer.add_data(
+            self.raw_dir.read_nexus('109SinoTibetanLanguages_Swadesh100-matched.nex'),
+            self.characters, 
+            args.log)
