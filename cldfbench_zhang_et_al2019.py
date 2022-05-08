be561b312e52a49c23e7d7e41a43a44a0d1c0632
SimonGreenhill <simon@simon.net.nz>
2022-05-08 20:55:02 +1200

    update

--- cldfbench_zhang_et_al2019.py
+++ cldfbench_zhang_et_al2019.py
@@ -9,24 +9,20 @@ class Dataset(phlorest.Dataset):
     
     def cmd_makecldf(self, args):
         self.init(args)
+
         args.writer.add_summary(
             self.raw_dir.read_tree(
                 '109SinoTibetanLanguages.MCC.tree', detranslate=True),
             self.metadata,
             args.log)
 
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('Sino-Tibetan Posterior tree distribution.trees.gz'),
-                1000),
-            detranslate=True,
-            as_nexus=True)
-        args.writer.add_posterior(
-            posterior.trees.trees, 
-            self.metadata, 
-            args.log)
+        posterior = self.raw_dir.read_trees(
+            'Sino-Tibetan Posterior tree distribution.trees.gz',
+            burnin=1000, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
 
         args.writer.add_data(
             self.raw_dir.read_nexus('109SinoTibetanLanguages_Swadesh100-matched.nex'),
             self.characters, 
             args.log)
+    
